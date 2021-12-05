884ee09d7a16e11563642d86563fa3403f84382e
Asadullah Hamidi <asad.hamidi119@gmail.com>
2021-12-05 17:50:39 +0530

    Content added. Strucutures amended.
    Added dynamic view.

--- script.js
+++ script.js
@@ -2,37 +2,46 @@ let btn = document.querySelectorAll(".link");
 let sections = document.querySelectorAll("section");
 let title = document.querySelector(".title");
 let theme = document.getElementById("theme");
-
+let menu = document.querySelector(".links-mobile");
+let menuChk = document.getElementById("menu-check");
 
 window.onscroll = () => {
-    let current = window.scrollY;
-    let id = "";
-
-    if(current > 500) 
+    if(window.innerWidth > 666)
     {
-        document.querySelector(".sidenav").classList.add("sidenav-active");
-        title.classList.add("title-scroll");
-        theme.classList.add("theme-scroll");
+        let current = window.scrollY;
+        let id = "";
+        if(current > 300) 
+        {
+            document.querySelector(".sidenav").classList.add("sidenav-active");
+            title.classList.add("title-scroll");
+            theme.classList.add("theme-scroll");
+        }
+        else
+        {
+            document.querySelector(".sidenav").classList.remove("sidenav-active");
+            title.classList.remove("title-scroll");
+            theme.classList.remove("theme-scroll");
+        } 
+            
+    
+        for(let i = 0; i < sections.length; i++)
+        {
+            if(current + 300 >= sections[i].offsetTop)
+                id = i;
+        }
+    
+        for(let i = 0; i < btn.length; i++)
+        {
+            btn[i].classList.remove("active");
+            if(i == id)
+            btn[i].classList.add("active");
+        }
     }
     else
     {
         document.querySelector(".sidenav").classList.remove("sidenav-active");
         title.classList.remove("title-scroll");
         theme.classList.remove("theme-scroll");
-    } 
-        
-
-    for(let i = 1; i < sections.length; i++)
-    {
-        if(current + 300 >= sections[i].offsetTop)
-            id = i;
-    }
-
-    for(let i = 0; i < btn.length; i++)
-    {
-        btn[i].classList.remove("active");
-        if(i == id - 1)
-        btn[i].classList.add("active");
     }
 
 };
@@ -46,4 +55,16 @@ function changeTheme(e)
     theme.src = "./assets/night.svg";
     else
     theme.src = "./assets/day.svg";
+}
+
+function openMenu()
+{
+    console.log("he");
+    menu.classList.toggle("menu-active");
+    
+    if(menuChk.checked)
+    menuChk.checked = false;
+    else
+    menuChk.checked = true;
+
 }

584240189667ee9fc3c970c052379653f11b0235
Asadullah Hamidi <asad.hamidi119@gmail.com>
2021-12-06 20:50:03 +0530

    Added documentation.
    Some minor UI changes.

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ let theme = document.getElementById("theme");
 let menu = document.querySelector(".links-mobile");
 let menuChk = document.getElementById("menu-check");
 
+
+// Sidenav functionality.
 window.onscroll = () => {
     if(window.innerWidth > 666)
     {
         let current = window.scrollY;
         let id = "";
+
+        // Displaying sidenav and title as we pass the front page.
         if(current > 300) 
         {
             document.querySelector(".sidenav").classList.add("sidenav-active");
@@ -23,22 +27,29 @@ window.onscroll = () => {
             theme.classList.remove("theme-scroll");
         } 
             
-    
+        // Looping through the sections list.
         for(let i = 0; i < sections.length; i++)
         {
+            /** This will end up with the last section 
+             * which is within the limit (current + 300) 
+                after looping through all the sections.**/
             if(current + 300 >= sections[i].offsetTop)
                 id = i;
         }
     
         for(let i = 0; i < btn.length; i++)
         {
+            // Removes the active class from all sidenav buttons.
             btn[i].classList.remove("active");
+
+            // And puts it on the current visible section.
             if(i == id)
             btn[i].classList.add("active");
         }
     }
     else
     {
+        // Removing sidenav, title, and theme button from left side when on front page.
         document.querySelector(".sidenav").classList.remove("sidenav-active");
         title.classList.remove("title-scroll");
         theme.classList.remove("theme-scroll");
@@ -46,10 +57,10 @@ window.onscroll = () => {
 
 };
 
+// Theme changing function.
 function changeTheme(e)
 {
     document.body.classList.toggle('dark-theme'); 
-    console.log("Check");
 
     if(document.body.classList == "dark-theme") 
     theme.src = "./assets/night.svg";
@@ -57,11 +68,11 @@ function changeTheme(e)
     theme.src = "./assets/day.svg";
 }
 
+// Function to open nav menu on smaller screens.
 function openMenu()
 {
-    console.log("he");
     menu.classList.toggle("menu-active");
-    
+
     if(menuChk.checked)
     menuChk.checked = false;
     else

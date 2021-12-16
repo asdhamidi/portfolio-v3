ca7407bbba9cd2b1b365410ce1abff2135ec8b8c
Asadullah Hamidi <asad.hamidi119@gmail.com>
2021-12-16 22:23:48 +0530

    Minor improvements and additions.

--- script.js
+++ script.js
@@ -14,10 +14,35 @@ let root = document.querySelector(":root");
 // Fade-in on scroll functionality.
 window.onscroll = () => {
     let current = window.scrollY;
+    let id = "";
+
+    // Looping through the sections list.
+    for(let i = 0; i < sections.length; i++)
+    {
+        /** This will end up with the last section 
+         * which is within the limit (current + 300) 
+            after looping through all the sections.**/
+        if(current + 300 >= sections[i].offsetTop)
+            id = i;
+    }
+
+    for(let i = 0; i < btn.length; i++)
+    {
+        // Removes the active class from all sidenav buttons.
+        btn[i].classList.remove("active");
+
+        // And puts it on the current visible section.
+        if(i == id)
+        btn[i].classList.add("active");
+    }
+
+    if(current > window.innerHeight * 0.75) 
+    document.querySelector(".sidenav").classList.add("sidenav-active");
+    else
+    document.querySelector(".sidenav").classList.remove("sidenav-active");
+
     if(window.innerWidth > 666)
     {
-        let id = "";
-
         // Displaying sidenav and title as we pass the front page.
         if(current > 300) 
         {
@@ -32,31 +57,10 @@ window.onscroll = () => {
             title.classList.remove("title-scroll");
             theme.classList.remove("theme-scroll");
         } 
-            
-        // Looping through the sections list.
-        for(let i = 0; i < sections.length; i++)
-        {
-            /** This will end up with the last section 
-             * which is within the limit (current + 300) 
-                after looping through all the sections.**/
-            if(current + 300 >= sections[i].offsetTop)
-                id = i;
-        }
-    
-        for(let i = 0; i < btn.length; i++)
-        {
-            // Removes the active class from all sidenav buttons.
-            btn[i].classList.remove("active");
-
-            // And puts it on the current visible section.
-            if(i == id)
-            btn[i].classList.add("active");
-        }
     }
     else
     {
         // Removing sidenav, title, and theme button from left side when on front page.
-        document.querySelector(".sidenav").classList.remove("sidenav-active");
         title.classList.remove("title-scroll");
         theme.classList.remove("theme-scroll");
 
